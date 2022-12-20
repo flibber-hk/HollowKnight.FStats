094e63b92f9c09a34fbc7daa7bb510c385c5e218
flibber-hk <76987839+flibber-hk@users.noreply.github.com>
2022-12-20 18:22:30 +0000

    Move DisplayInfo to a separate file
    This is not binary breaking as the namespace is the same

--- FStats/StatController.cs
+++ FStats/StatController.cs
@@ -2,31 +2,6 @@
 
 namespace FStats
 {
-    public class DisplayInfo
-    {
-        /// <summary>
-        /// The title of the display. Must not be null or empty.
-        /// </summary>
-        public string Title { get; set; }
-        /// <summary>
-        /// Text shown below the title. May be empty - will not be shown if so.
-        /// </summary>
-        public string MainStat { get; set; }
-        /// <summary>
-        /// The text in each column.
-        /// </summary>
-        public List<string> StatColumns { get; set; }
-        /// <summary>
-        /// Stat screens will be cycled in increasing order of priority.
-        /// </summary>
-        public double Priority { get; set; } = 0;
-
-        /// <summary>
-        /// Creates and returns a shallow copy of this DisplayInfo object.
-        /// </summary>
-        public DisplayInfo Clone() => (DisplayInfo)MemberwiseClone();
-    }
-
     public abstract class StatController
     {
         /// <summary>
