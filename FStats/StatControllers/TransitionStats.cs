38314be388c70ecc04dd9a13784f8e9ae572b7ab
flibber-hk <76987839+flibber-hk@users.noreply.github.com>
2022-12-29 10:19:56 +0000

    Track more transition types

--- FStats/StatControllers/TransitionStats.cs
+++ FStats/StatControllers/TransitionStats.cs
@@ -9,29 +9,60 @@ namespace FStats.StatControllers
 {
     public class TransitionStats : StatController
     {
-        public Dictionary<GatePosition, int> TransitionsEntered = CollectionUtils.EmptyCounter<GatePosition>();
-        public Dictionary<GatePosition, int> TransitionsExited = CollectionUtils.EmptyCounter<GatePosition>();
+        public enum TransitionType
+        {
+            Left,
+            Right,
+            Top,
+            Bottom,
+            Door,
+            Dream,
+            Dreamgate,
+            Stag,
+            Other
+        }
+
+        public Dictionary<TransitionType, int> TransitionsEntered = CollectionUtils.EmptyCounter<TransitionType>();
+        public Dictionary<TransitionType, int> TransitionsExited = CollectionUtils.EmptyCounter<TransitionType>();
 
         public float TransitionTime;
 
-        public static GatePosition NameToDirection(string transition)
+        public static TransitionType NameToDirection(string transition)
         {
-            if (transition is null) return GatePosition.unknown;
+            if (transition is null) return TransitionType.Other;
 
-            if (transition.Contains("left")) return GatePosition.left;
-            if (transition.Contains("right")) return GatePosition.right;
-            if (transition.Contains("top")) return GatePosition.top;
-            if (transition.Contains("bot")) return GatePosition.bottom;
+            if (transition.Contains("left")) return TransitionType.Left;
+            if (transition.Contains("right")) return TransitionType.Right;
+            if (transition.Contains("top")) return TransitionType.Top;
+            if (transition.Contains("bot")) return TransitionType.Bottom;
 
-            if (transition.Contains("door") || transition.StartsWith("room")) return GatePosition.door;
+            if (transition.Contains("door_stagExit")) return TransitionType.Stag;
+            if (transition.Contains("door_dreamReturn")) return TransitionType.Dream;
+            if (transition.ToLower().Contains("dreamgate")) return TransitionType.Dreamgate;
+
+            // Not sure
+            // if (transition.Contains("door") || transition.StartsWith("room")) return TransitionType.Door;
             
             // There are a lot of ways it can be a door
-            return GatePosition.unknown;
+            return TransitionType.Other;
         }
+
+        public static TransitionType FromGatePosition(GatePosition pos) => pos switch
+        {
+            GatePosition.right => TransitionType.Right,
+            GatePosition.left => TransitionType.Left,
+            GatePosition.bottom => TransitionType.Bottom,
+            GatePosition.top => TransitionType.Top,
+            GatePosition.door => TransitionType.Door,
+            _ => TransitionType.Other
+        };
 
         public override void Initialize()
         {
+            _recordedLastTransition = null;
+
             On.GameManager.BeginSceneTransition += RecordTransitions;
+            On.HutongGames.PlayMaker.Actions.BeginSceneTransition.OnEnter += NotifyDoorTransitions;
 
             ModHooks.HeroUpdateHook += RecordTransitionTime;
         }
@@ -39,22 +70,57 @@ namespace FStats.StatControllers
         public override void Unload()
         {
             On.GameManager.BeginSceneTransition -= RecordTransitions;
+            On.HutongGames.PlayMaker.Actions.BeginSceneTransition.OnEnter -= NotifyDoorTransitions;
 
             ModHooks.HeroUpdateHook -= RecordTransitionTime;
+        }
+
+        private TransitionType? _recordedLastTransition = null;
+
+        private void NotifyDoorTransitions(On.HutongGames.PlayMaker.Actions.BeginSceneTransition.orig_OnEnter orig, HutongGames.PlayMaker.Actions.BeginSceneTransition self)
+        {
+            if (self.Fsm.FsmComponent.FsmName == "Door Control")
+            {
+                _recordedLastTransition = TransitionType.Door;
+            }
+            else if (self.Fsm.FsmComponent.gameObject.name.StartsWith("Dream Enter"))
+            {
+                _recordedLastTransition = TransitionType.Dream;
+            }
+
+            orig(self);
         }
 
         private void RecordTransitions(On.GameManager.orig_BeginSceneTransition orig, GameManager self, GameManager.SceneLoadInfo info)
         {
             orig(self, info);
 
-            // Assume this is a warp of some sort
+            // Assume this is a warp of some sort (e.g. Benchwarp, Debug save state, so we don't need to load it)
             if (info.GetType() != typeof(GameManager.SceneLoadInfo)) return;
 
-            GatePosition source = info.HeroLeaveDirection ?? GatePosition.unknown;
-            TransitionsEntered[source]++;
+            TransitionType source = FromGatePosition(info.HeroLeaveDirection ?? GatePosition.unknown);
+            TransitionType target = NameToDirection(info.EntryGateName);
 
-            GatePosition target = NameToDirection(info.EntryGateName);
+            if (_recordedLastTransition.HasValue)
+            {
+                source = _recordedLastTransition.Value;
+                _recordedLastTransition = null;
+            }
+
+            if (target == TransitionType.Stag || target == TransitionType.Dreamgate || target == TransitionType.Dream)
+            {
+                source = target;
+            }
+            else if (source == TransitionType.Stag || source == TransitionType.Dreamgate || source == TransitionType.Dream)
+            {
+                target = source;
+            }
+
+            TransitionsEntered[source]++;
             TransitionsExited[target]++;
+
+            FStatsMod.instance.Log($"{source} {target} {info.SceneName} {info.EntryGateName} {info.GetType()}");
+            FStatsMod.instance.Log("");
         }
 
         private void RecordTransitionTime()
@@ -70,31 +136,28 @@ namespace FStats.StatControllers
 
         public override IEnumerable<DisplayInfo> GetDisplayInfos()
         {
-            string LeftCol = new StringBuilder()
+            StringBuilder leftCol = new StringBuilder()
                 .AppendLine($"Transitions Entered: {TransitionsEntered.Values.Sum()}")
-                .AppendLine()
-                .AppendLine($"Left: {TransitionsEntered[GatePosition.left]}")
-                .AppendLine($"Right: {TransitionsEntered[GatePosition.right]}")
-                .AppendLine($"Top: {TransitionsEntered[GatePosition.top]}")
-                .AppendLine($"Bottom: {TransitionsEntered[GatePosition.bottom]}")
-                .AppendLine($"Other: {TransitionsEntered[GatePosition.door] + TransitionsEntered[GatePosition.unknown]}")
-                .ToString();
+                .AppendLine();
+            foreach ((TransitionType t, int count) in TransitionsEntered)
+            {
+                leftCol.AppendLine($"{t}: {count}");
+            }
 
-            string RightCol = new StringBuilder()
+            StringBuilder rightCol = new StringBuilder()
                 .AppendLine($"Transitions Exited: {TransitionsExited.Values.Sum()}")
-                .AppendLine()
-                .AppendLine($"Left: {TransitionsExited[GatePosition.left]}")
-                .AppendLine($"Right: {TransitionsExited[GatePosition.right]}")
-                .AppendLine($"Top: {TransitionsExited[GatePosition.top]}")
-                .AppendLine($"Bottom: {TransitionsExited[GatePosition.bottom]}")
-                .AppendLine($"Other: {TransitionsExited[GatePosition.door] + TransitionsExited[GatePosition.unknown]}")
-                .ToString();
+                .AppendLine();
+            foreach ((TransitionType t, int count) in TransitionsExited)
+            {
+                rightCol.AppendLine($"{t}: {count}");
+            }
+
 
             yield return new()
             {
                 Title = "Transition Stats",
                 MainStat = $"Time transitioning: {Common.Instance.GetTimePercentString(TransitionTime)}",
-                StatColumns = new() { LeftCol, RightCol },
+                StatColumns = new() { leftCol.ToString(), rightCol.ToString() },
                 Priority = BuiltinScreenPriorityValues.TransitionStats,
             };
         }
