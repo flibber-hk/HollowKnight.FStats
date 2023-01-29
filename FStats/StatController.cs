71acbca377b78f2423a94efdcfc73e7e17781289
flibber-hk <76987839+flibber-hk@users.noreply.github.com>
2023-01-29 16:19:13 +0000

    Changes to StatController for global stats

--- FStats/StatController.cs
+++ FStats/StatController.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace FStats
 {
@@ -7,14 +10,27 @@ namespace FStats
         /// <summary>
         /// Run when entering the save file.
         /// </summary>
-        public abstract void Initialize();
+        public virtual void Initialize() { }
         /// <summary>
         /// Run when leaving the save file - `() => { Initialize(); Unload(); };` should be the identity.
         /// </summary>
-        public abstract void Unload();
+        public virtual void Unload() { }
+
         /// <summary>
         /// Yield a DisplayInfo object for each page that the StatController wants to display.
+        /// This method will be called if this StatController is associated with the Local Settings.
         /// </summary>
-        public abstract IEnumerable<DisplayInfo> GetDisplayInfos();
+        public virtual IEnumerable<DisplayInfo> GetDisplayInfos() => Enumerable.Empty<DisplayInfo>();
+
+        /// <summary>
+        /// Yield a DisplayInfo object for each page that the StatController wants to display.
+        /// This method will be called if this StatController is associated with the Global Settings.
+        /// </summary>
+        public virtual IEnumerable<DisplayInfo> GetGlobalDisplayInfos() => Enumerable.Empty<DisplayInfo>();
+
+        /// <summary>
+        /// The number of save files this StatController has been initialized with.
+        /// </summary>
+        [JsonProperty] public int FileCount { get; internal set; } = 0;
     }
 }
