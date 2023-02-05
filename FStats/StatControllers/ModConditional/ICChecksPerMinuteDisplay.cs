899f497bfb2a66823d5ad13d532f1da7d69c764f
flibber-hk <76987839+flibber-hk@users.noreply.github.com>
2023-02-05 08:31:23 +0000

    Remove some usings

--- FStats/StatControllers/ModConditional/ICChecksPerMinuteDisplay.cs
+++ FStats/StatControllers/ModConditional/ICChecksPerMinuteDisplay.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using FStats.Util;
 using ItemChanger;
 using ItemChanger.Placements;
